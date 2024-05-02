45b616a1bf483fa71c18f1129c391d1c0144a646
zohairahmedd <151594538+zohairahmedd@users.noreply.github.com>
2024-05-02 18:48:12 -0400

    new buttons & finished updated layout

--- calculator.py
+++ calculator.py
@@ -2,9 +2,9 @@ from tkinter import *
 
 root = Tk() # creates main window of an application
 root.title("Calculator") # sets title of main window
-root.configure(bg="#808080") # sets background colour for main window
+root.configure(bg="#000000") # sets background colour for main window
 
-e = Entry(root, width=21, borderwidth=2, bg="#808080", fg="white", font=("Helvetica", 22, "bold")) # defines characteristics of widget e - characteristics inside the window
+e = Entry(root, width=22, borderwidth=2, bg="#000000", fg="white", font=("Helvetica", 22, "bold")) # defines characteristics of widget e - characteristics inside the window
 e.grid(row=0, column=0, columnspan=4, padx=4, pady=5, ipadx=8, ipady=13) # places widget inside the main window with padding
 
 def button_click(number): # function button_click with argument number
@@ -61,25 +61,25 @@ def button_equal(): # function button_equal with no argument
         e.insert(0, f_num / float(second_number)) # divides first and second number if math is division
 
 # changing the look of each individual button on the calculator (colour, padding, lambda: takes any number of arguments, but can only have one expression)
-button_1 = Button(root, text='1',bg="#4a503d",fg="white", padx=40, pady=20, command=lambda: button_click(1))
-button_2 = Button(root, text='2',bg="#4a503d",fg="white", padx=41, pady=20, command=lambda: button_click(2))
-button_3 = Button(root, text='3',bg="#4a503d",fg="white", padx=39, pady=20, command=lambda: button_click(3))
-button_4 = Button(root, text='4',bg="#4a503d",fg="white", padx=40, pady=20, command=lambda: button_click(4))
-button_5 = Button(root, text='5',bg="#4a503d",fg="white", padx=41, pady=20, command=lambda: button_click(5))
-button_6 = Button(root, text='6',bg="#4a503d",fg="white", padx=39, pady=20, command=lambda: button_click(6))
-button_7 = Button(root, text='7',bg="#4a503d",fg="white", padx=40, pady=20, command=lambda: button_click(7))
-button_8 = Button(root, text='8',bg="#4a503d",fg="white", padx=41, pady=20, command=lambda: button_click(8))
-button_9 = Button(root, text='9',bg="#4a503d",fg="white", padx=39, pady=20, command=lambda: button_click(9))
-button_0 = Button(root, text='0',bg="#4a503d",fg="white", padx=87, pady=20, command=lambda: button_click(0))
-button_dot = Button(root, text='.',bg="#4a503d",fg="white", padx=42.5, pady=20, command=lambda: button_click('.'))
-button_add = Button(root, text='+',bg="#4a503d",fg="white", padx=39, pady=20, command= button_add)
-button_equal = Button(root, text='=',bg="#4a503d",fg="white", padx=87, pady=20, command= button_equal)
-button_clear = Button(root, text='AC',bg="#4a503d",fg="white", padx=34, pady=20, command= button_clear)
-button_subtract = Button(root, text='-',bg="#4a503d",fg="white", padx=41, pady=20, command= button_subtract)
-button_multiply = Button(root, text='*',bg="#4a503d",fg="white", padx=42, pady=20, command= button_multiply)
-button_divide = Button(root, text='/',bg="#4a503d",fg="white", padx=40, pady=20, command= button_divide)
+button_1 = Button(root, text='1',bg="#404040",fg="white", padx=40, pady=20, command=lambda: button_click(1))
+button_2 = Button(root, text='2',bg="#404040",fg="white", padx=43, pady=20, command=lambda: button_click(2))
+button_3 = Button(root, text='3',bg="#404040",fg="white", padx=48, pady=20, command=lambda: button_click(3))
+button_4 = Button(root, text='4',bg="#404040",fg="white", padx=40, pady=20, command=lambda: button_click(4))
+button_5 = Button(root, text='5',bg="#404040",fg="white", padx=43, pady=20, command=lambda: button_click(5))
+button_6 = Button(root, text='6',bg="#404040",fg="white", padx=48, pady=20, command=lambda: button_click(6))
+button_7 = Button(root, text='7',bg="#404040",fg="white", padx=40, pady=20, command=lambda: button_click(7))
+button_8 = Button(root, text='8',bg="#404040",fg="white", padx=43, pady=20, command=lambda: button_click(8))
+button_9 = Button(root, text='9',bg="#404040",fg="white", padx=48, pady=20, command=lambda: button_click(9))
+button_0 = Button(root, text='0',bg="#404040",fg="white", padx=91, pady=20, command=lambda: button_click(0))
+button_dot = Button(root, text='.',bg="#404040",fg="white", padx=49, pady=20, command=lambda: button_click('.'))
+button_add = Button(root, text='+',bg="#d47f11",fg="white", padx=40, pady=20, command= button_add)
+button_equal = Button(root, text='=',bg="#d47f11",fg="white", padx=90, pady=20, command= button_equal)
+button_clear = Button(root, text='AC',bg="#d47f11",fg="white", padx=91, pady=20, command= button_clear)
+button_subtract = Button(root, text='-',bg="#d47f11",fg="white", padx=42, pady=20, command= button_subtract)
+button_multiply = Button(root, text='*',bg="#d47f11",fg="white", padx=42, pady=20, command= button_multiply)
+button_divide = Button(root, text='/',bg="#d47f11",fg="white", padx=42, pady=20, command= button_divide)
 
-# positions number buttons at a specific location in the grid defined by row and column parameters | notice how nothing is in the same row & column
+# positions number buttons at a specific location in the grid defined by row and column parameters
 button_1.grid(row=3, column=0)
 button_2.grid(row=3, column=1)
 button_3.grid(row=3, column=2)
@@ -89,13 +89,13 @@ button_6.grid(row=2, column=2)
 button_7.grid(row=1, column=0)
 button_8.grid(row=1, column=1)
 button_9.grid(row=1, column=2)
-button_0.grid(row=4, column=0, columnspan=3)
+button_0.grid(row=4, column=0, columnspan=2)
 button_dot.grid(row=4, column=2)
 
-# positions operation buttons at a specific location in the grid defined by row and column parameters | notice how nothing is in the same row & column
-button_clear.grid(row=5, column=3)
+# positions operation buttons at a specific location in the grid defined by row and column parameters
+button_clear.grid(row=5, column=2, columnspan=2)
 button_add.grid(row=4, column=3)
-button_equal.grid(row=5, column=1, columnspan=2)
+button_equal.grid(row=5, column=0, columnspan=2)
 button_subtract.grid(row=3, column=3)
 button_multiply.grid(row=2, column=3)
 button_divide.grid(row=1, column=3)
